72147468dc35ecd2859d0ae5224a4cdd27670589
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-10-11 21:50:02 -0600

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const { createProxyMiddleware } = require("http-proxy-middleware")
+
 module.exports = {
-  /* Your site config here */
+  // for avoiding CORS while developing Netlify Functions locally
+  // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
+  developMiddleware: app => {
+    app.use(
+      "/.netlify/functions/",
+      createProxyMiddleware({
+        target: "http://localhost:9000",
+        pathRewrite: {
+          "/.netlify/functions/": "",
+        },
+      })
+    )
+  },
+
   siteMetadata: {
     title: "Tiya Gallegos", 
     author: "Tiya Gallegos",
